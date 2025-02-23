374000270f2cda2a0484cd359a61cd96948b79ac
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-23 15:27:32 +0100

    Added Register Method

--- Join.API/Controllers/AuthController.cs
+++ Join.API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Join.API.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Join.API.Controllers
@@ -7,5 +9,41 @@ namespace Join.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDTO.Username,
+                Email = registerRequestDTO.Username
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDTO.Password);
+
+            if (identityResult.Succeeded)
+            {
+                if (registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDTO.Roles);
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User was registered.");
+                    }
+                }
+            }
+
+            return BadRequest();
+        }
+
+
     }
 }
