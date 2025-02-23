b6a04db51368a43a6513728ca45b12e7c78b9b0d
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-23 14:24:30 +0100

    Added SubtasksController

---
+++ Join.API/Controllers/SubtasksController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Join.API.Models.Domain;
+using Join.API.Models.DTOs;
+using Join.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Join.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubtasksController : ControllerBase
+    {
+        private readonly ISubtaskRepository subtaskRepository;
+        private readonly IMapper mapper;
+
+        public SubtasksController(ISubtaskRepository subtaskRepository, IMapper mapper)
+        {
+            this.subtaskRepository = subtaskRepository;
+            this.mapper = mapper;
+        }
+
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateSubtaskDTO updateSubtaskDTO)
+        {
+            var subtaskDomainModel = mapper.Map<Subtask>(updateSubtaskDTO);
+            subtaskDomainModel = await subtaskRepository.UpdateAsync(id, subtaskDomainModel);
+
+            if(subtaskDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            var subtaskDTO = mapper.Map<SubtaskDTO>(subtaskDomainModel);
+            return Ok(subtaskDTO);
+        }
+
+    }
+}
